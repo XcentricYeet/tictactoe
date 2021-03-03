9108b7c17c9bd6f2cb50779510e20812e1590dc1
River <river.creech27@gmail.com>
2021-03-03 02:11:15 -0600

    Completed everything but AI, began AI

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 
 void printBoard(char board[]);
 bool checkWin(char board[], char ID);
-void local();
-void AI();
+void localGame();
+int AI(char board[], char ID, char playerID);
+void AIGame();
 
 int main() {
 	int style=0;
@@ -13,9 +14,9 @@ int main() {
 
 	try {
 		if(style==1) {
-			local();
+			localGame();
 		} else if(style==2) {
-			AI();
+			AIGame();
 		} else {
 			std::cout << "Error... Please make sure to choose either 1 or 2!" << std::endl;
 		}
@@ -35,7 +36,7 @@ bool checkWin(char board[], char ID) {
 	return false;
 }
 
-void local() {
+void localGame() {
 	char board[9] = "        ";
 	board[8] = ' ';
 	char ID = 'x';
@@ -81,6 +82,75 @@ void local() {
 	}
 }
 
-void AI() {
-	return;
+int AI(char board[], char ID, char playerID) {
+	if((board[0]==' ' && (board[1]==ID||board[1]==playerID) && (board[2]==ID || board[2]==playerID)) || (board[0]==' ' && (board[4]==ID || board[4]==playerID) && (board[8]==ID || board[8]==playerID)) || (board[0]==' ' && (board[3]==ID || board[3]==playerID) && (board[6]==ID || board[6]==playerID))) {
+		return 1;
+	}
+}
+
+void AIGame() {
+	char board[9] = "        ";
+	board[8] = ' ';
+	char ID = 'x';
+	char playerID = ' ';
+	int i=0;
+	bool win = false;
+
+	std::cout << "Would you rather be x or o?" << std::endl;
+	std::cin >> playerID;
+
+	try {
+		if(playerID!='x' && playerID!='o') {
+			std::cout << "Error... Please make sure to choose a valid option" << std::endl;
+			return;
+		}
+	} catch(...) {
+		std::cout << "Error... Please make sure to choose a valid option" << std::endl;
+		return;
+	}
+
+	while(i<9) {
+		printBoard(board);
+		int move=0;
+		std::cout << "player " << ID << "'s turn\nenter a number(1-9)" << std::endl;
+		if(ID==playerID) {
+			std::cin >> move;
+		} else {
+			move = AI(board, ID, playerID);
+		}
+
+		try {
+			if(move>=1 && move<=9) {
+				if(board[move-1]!= ' ') {
+					std::cout << "That spot is occupied!" << std::endl;
+				} else {
+					board[move-1] = ID;
+					if(checkWin(board, ID)) {
+						win=true;
+						break;
+					}
+					if(ID == 'x') {
+						ID = 'y';
+					} else {
+						ID = 'x';
+					}
+					i++;
+				}
+			} else {
+				std::cout << "That number does not correlate to a space on the board" << std::endl;
+			}
+		} catch (...) {
+			std::cout << "Error... Please make sure you entered an integer value!" << std::endl;
+		}
+	}
+
+	std::cout << "Printing finalized board..." << std::endl;
+	printBoard(board);
+	if(!win){
+		std::cout << "This game resulted in a tie! Nobody wins!";
+	} else if(ID==playerID) {
+		std::cout << "You won the game!";
+	} else {
+		std::cout << "The AI won the game!";
+	}
 }
